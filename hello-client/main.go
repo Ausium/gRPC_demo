badc43044baf53018f8fd42da895c3d8a09cd825
Autism <814844299@qq.com>
2023-07-12 00:05:35 +0800

    feat:token认证

--- hello-client/main.go
+++ hello-client/main.go
@@ -6,11 +6,37 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	pb "grpc_demo/hello-server/proto"
+	"time"
 )
 
+// 自定义token认证代码
+type ClientTokenAuth struct {
+}
+
+func (c ClientTokenAuth) GetRequestMetadata(ctx context.Context, url ...string) (map[string]string, error) {
+	return map[string]string{
+		"appId":  "grpcstudy",
+		"appKey": "123456",
+	}, nil
+}
+
+func (c ClientTokenAuth) RequireTransportSecurity() bool {
+	return false
+}
+
 func main() {
-	//连接到server端，此处禁用安全传输，没有加密和验证
-	conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	start := time.Now() // 记录开始时间
+
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts, grpc.WithPerRPCCredentials(new(ClientTokenAuth)))
+
+	//creds, _ := credentials.NewClientTLSFromFile("D:\\Environment\\GoWorks\\gRPC_demo\\key\\test.pem", "*.grpcstudy.com")
+
+	//连接到server端，此处禁用安全传输，没有加密和验证`
+	conn, err := grpc.Dial("127.0.0.1:9090", opts...)
+	//conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		fmt.Printf("did not connect: %v", err)
 	}
@@ -21,6 +47,7 @@ func main() {
 	//执行grpc调用（这个方法在服务端来实现并返回结果）
 	resp, _ := client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "chen"})
 
-	fmt.Printf("返回的结果为：%v", resp)
-
+	fmt.Printf("返回的结果为：%v\n", resp)
+	endTime := time.Since(start)
+	fmt.Printf("程序耗时时间为: %v", endTime)
 }
--- hello-server/main.go
+++ hello-server/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	pb "grpc_demo/hello-server/proto"
 	"net"
 )
@@ -13,15 +16,46 @@ type server struct {
 }
 
 func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+
+	//获取元数据的信息
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("未传输token！")
+	}
+	var appId string
+	var appKey string
+	if v, ok := md["appid"]; ok {
+		appId = v[0]
+		fmt.Println("appId", appId)
+	}
+
+	if v, ok := md["appkey"]; ok {
+		appKey = v[0]
+		fmt.Println("appKey", appKey)
+	}
+
+	//正常这里也是从数据库中查找，每个用户都会有一个自己的appId
+	//然后根据用户的id来查找 appId，然后再校验这appId是否正确，不正确就不让通过
+	if appId != "grpcstudy" || appKey != "123456" {
+		return nil, errors.New("token校验失败！")
+	}
+
+	fmt.Println("hello" + req.RequestName)
 	return &pb.HelloResponse{ResponseMsg: "hello" + req.RequestName}, nil
 }
 
 func main() {
+
+	//TSL认证
+	//两个参数分别是 cretFile, keyFile
+	//自签名证书文件和私钥文件
+	//creds, _ := credentials.NewServerTLSFromFile("D:\\Environment\\GoWorks\\gRPC_demo\\key\\test.pem", "D:\\Environment\\GoWorks\\gRPC_demo\\key\\test.key")
+
 	//开启端口
 	listen, _ := net.Listen("tcp", ":9090")
 
 	//创建grpc服务
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
 	//在grpc服务端中去注册我们自己编写的服务
 	pb.RegisterSayHelloServer(grpcServer, &server{})
 
